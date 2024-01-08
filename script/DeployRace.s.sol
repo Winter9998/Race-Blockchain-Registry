905da32757493ac662c1f5305477ab794ce5b9b0
Younes <younesgt350@gmail.com>
2024-01-08 11:52:25 +0100

    scripts testingk, commit changes

--- script/DeployRace.s.sol
+++ script/DeployRace.s.sol
@@ -1,12 +1,15 @@
 // SPDX-License-Identifier: UNLICENSED
-pragma solidity ^0.8.13;
+pragma solidity ^0.8.15;
 
 import {Script, console2} from "forge-std/Script.sol";
+import "forge-std/console.sol";
+import "../src/RaceMarathon.sol";
 
-contract CounterScript is Script {
-    function setUp() public {}
+contract DeployRace is Script {
+    RaceMarathon marathon;
 
     function run() public {
         vm.broadcast();
+        marathon = new RaceMarathon();
     }
 }
---
+++ script/Interactions.s.sol
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.15;
+
+import {Script, console2} from "forge-std/Script.sol";
+import "forge-std/console.sol";
+import "../src/RaceMarathon.sol";
+
+contract Interactions is Script {
+    RaceMarathon marathon;
+
+    function setUp() public {
+        marathon = RaceMarathon(payable(0x5FbDB2315678afecb367f032d93F642f64180aa3));
+    }
+    
+    function run() public {
+        getFee();
+    }
+
+    function getFee() public {
+        vm.startBroadcast();
+        uint256 res = marathon.getEntranceFee("Discount");
+        console.log(res);
+        vm.stopBroadcast();
+    }
+
+        // Private key : 0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80
+        // Public Key : 0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266
+
+}
